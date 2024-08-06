dea68e8ef5c813a736325f0e00084d9148753302
Mandy Lewerenz <m.lewerenz@reply.de>
2024-08-06 09:12:36 +0200

    styling - added Fontawesome - added icon to button - added generel styling - added button styling

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, EventEmitter, Injectable, Output } from '@angular/core';
 import { Quote } from '../../model/quote.model';
 import { SimpsonsService } from '../../service/quote.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
@@ -14,12 +16,13 @@ import { SimpsonsService } from '../../service/quote.service';
 
 export class ButtonComponent {
   @Input() text: string = "";
+  faMagnifyingGlass = faMagnifyingGlass;
 
   constructor(private simpsonsService: SimpsonsService) {}
 
   onClick = () : void => {
-    this.simpsonsService.getAllQuotes().subscribe((res: Quote[]) => {
-      this.simpsonsService.setQuotes(res);
+    this.simpsonsService.getAllQuotes().subscribe((res: Quote[]) => { //
+      this.simpsonsService.setQuotes(res); // Ergebnis aus Api Call
       }); 
   };
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from "../button/button.component";
-
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -11,7 +10,5 @@ import { ButtonComponent } from "../button/button.component";
   styleUrl: './navbar.component.scss',
 })
 
-
 export class NavbarComponent {
-  
 }
